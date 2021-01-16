fe5b070215a572a449828746562c76eab7fbf912
Eugene Chu <eugenchu@gmail.com>
2021-01-15 19:02:47 -0800

    calculate discount price and display on the screen correct amount to the correct fixed precision

--- client/component/Price.jsx
+++ client/component/Price.jsx
@@ -2,17 +2,21 @@ import React from 'react';
 
 let Price = (props) => {
     let {price, discount} = props.product;
-    let disPrice;
+    let disPrice, disAmount;
     if(price !== undefined){
         price = price.toFixed(2);
     }
     if(discount !== undefined && discount !== 0){
-        disPrice = (price*((100-discount)/100)).toFixed(2);
+        // should have seeded data with decimal values instead of whole number %
+        // would not have to deal with (100-discount/100) to get the price off.
+        // could simply do price * discount instead for disAmount
+        disAmount = discount/100*price;
+        disPrice = (price-disAmount).toFixed(2);
     }
     return (
         <>
-            <div>Price: {discount ? `$${disPrice} Sale` : price}</div>
-            <div>Discount: {discount}</div>
+            <div>Price: {discount ? `$${(price-disAmount).toFixed(2)} Sale` : price}</div>
+            <div>Discount: {discount ? `Save $${disAmount} (${discount}% Off)` : 'No Discount. Currently a placeholder'}</div>
         </>
     );
 }
