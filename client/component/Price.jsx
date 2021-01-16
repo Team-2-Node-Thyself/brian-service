83b9ed2a98a5857da5cce2c5c2bc0fd97ba37191
Eugene Chu <eugenchu@gmail.com>
2021-01-15 18:49:52 -0800

    implement a feature to check and display the price according to the discount amount

--- client/component/Price.jsx
+++ client/component/Price.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 
 let Price = (props) => {
-    var {price, discount} = props.product;
+    let {price, discount} = props.product;
+    let disPrice;
     if(price !== undefined){
         price = price.toFixed(2);
     }
+    if(discount !== undefined && discount !== 0){
+        disPrice = (price*((100-discount)/100)).toFixed(2);
+    }
     return (
         <>
-            <div>Price: {price}</div>
+            <div>Price: {discount ? `$${disPrice} Sale` : price}</div>
             <div>Discount: {discount}</div>
         </>
     );
