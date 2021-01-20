529020253e2bccc4ddf757984e3db930022a011f
Eugene Chu <eugenchu@gmail.com>
2021-01-20 15:43:28 -0800

    Trying to solve the CSS/Flexbox arrangement for the check out area. Committing to put on hold so I can move onto testing

--- client/component/CheckoutForm.jsx
+++ client/component/CheckoutForm.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import styled from 'styled-components';
 import style from 'styled-components';
 
 const Fulfillment = style.div`
     display: flex;
+    flex-direction: column;
     border: 5px dotted maroon;
+    background: lightgray;
 `;
 const FulfillmentButton = style.button`
     font-size: 15px;
@@ -22,18 +25,30 @@ const FulfillmentButton = style.button`
     display: inline-flex;
     justify-content: center;
     align-items: center;
+    padding-left: 4px !important;
 
+     &:hover{
+        background-color: rgb(170, 0, 0);
+        border-color: rgb(170, 0, 0);
+     };
     &:active{ 
         background-color: rgb(121, 0, 0);
         border-color: rgb(121, 0, 0);
-     }
+        outline: none;
+     };
+     &:focus{
+         outline: none;
+     };
 `;
+
+const FFAreaOne = styled.div``;
 
 function CheckoutForm(props) {
 
     return (
-        <Fulfillment>{props.children}
-        <FulfillmentButton>{props.buttonText}</FulfillmentButton>
+        <Fulfillment>
+            <FFAreaOne>{props.children}<FulfillmentButton>{props.buttonText}</FulfillmentButton></FFAreaOne>
+            <FFAreaTwo></FFAreaTwo>
         </Fulfillment>
     )
 };
--- client/component/PickUp.jsx
+++ client/component/PickUp.jsx
@@ -1,14 +1,63 @@
 import React from 'react';
-import style from 'styled-components';
+import styled from 'styled-components';
 
-const PickUpText = style.div`
-    flex-basis: 50%
+const PickUpTextArea1 = styled.div`
+    flex-basis: 50%;
+    padding-right 4px !important;
 `;
 
-function PickUp(props) {
+const PickUpTextArea2 = styled.div`
+    order: 3;
+    flex-wrap: wrap;
+    align-content: flex-end;
+`;
+
+const PUTitle = styled.span`
+    color: rgb(0, 102, 1) !important;
+    font-weight: bold !important;
+    padding-left: 5px;
+`;
+
+const PULocation = styled.button`
+    text-align: left;
+    font-weight: bold !important;
+    position: relative;
+    text-decoration: underline;
+    background-color: transparent;
+    border: none;
+    font: inherit;
+    &:focus{
+        outline: 1px dashed black;
+    }
+`;
+
+const PUEdit = styled.button`
+    text-decoration: underline !important;
+    font-size: 12px !important;
+    font: inherit;
+    border: none;
+    background-color: transparent;
+
+    &:focus{
+        outline: 1px dashed black;
+    }
+`;
+
+const PUReady = styled.div`
+    font-size: 14px !important;
+`;
+
+function PickUp() {
  return (
      <>
-    <PickUpText>this is the pick up form</PickUpText>
+    <PickUpTextArea1>
+        <PUTitle>Pick up today </PUTitle><span>at</span>
+        <PULocation>New United States Mars</PULocation>
+        <PUEdit>Edit store</PUEdit>
+    </PickUpTextArea1>
+    <PickUpTextArea2>
+        <PUReady>Ready within 2 hours for pickup inside the store.</PUReady>
+    </PickUpTextArea2>
     </>
  );
 };
