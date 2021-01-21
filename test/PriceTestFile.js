71f04e1d98f61d84fc412b570a824a4c8b8978de
Eugene Chu <eugenchu@gmail.com>
2021-01-20 16:56:22 -0800

    Add an extra (pseudo)test into PriceTestFile

--- test/PriceTestFile.js
+++ test/PriceTestFile.js
@@ -10,4 +10,6 @@ import Price from './client/component/Price.jsx';
  * I would check if it is not incorrectly displaying the price
  * --Make sure it does not try to display a discount price when there is no discount
  * --Make sure it does not try to display the normal price when there is a discount
+ * I would check if it display the correct amount of saving if there is a sales/discount
+ * I would check if the saving portion does not display if there is no sales/discount on the item
  */
