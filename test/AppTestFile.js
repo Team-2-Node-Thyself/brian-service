95b12d0207f691b2024b7056c209c4eb1b102527
Eugene Chu <eugenchu@gmail.com>
2021-01-20 16:50:50 -0800

    pseudocode/wrote out some explaination on what the app test and price test should consist of

--- test/AppTestFile.js
+++ test/AppTestFile.js
@@ -2,3 +2,9 @@ import React from 'react';
 import {shallow, mount, render} from 'enzyme';
 
 import App from './client/component/App.jsx';
+
+/**
+ * This is where I would test the App component.
+ * I would check if my app is getting the correct information from componentDidMount and doing the correct full render afterwards
+ * I would check if my app is not rendering the 'broken' div line that should only show if no data came back form the database
+ */
---
+++ test/PriceTestFile.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {shallow, mount, render} from 'enzyme';
+
+import Price from './client/component/Price.jsx';
+
+/**
+ * This is where I would test the Price component
+ * I would check if it is displaying the price
+ * I would check if it is displaying the price correctly if there is a sales/discount
+ * I would check if it is not incorrectly displaying the price
+ * --Make sure it does not try to display a discount price when there is no discount
+ * --Make sure it does not try to display the normal price when there is a discount
+ */
