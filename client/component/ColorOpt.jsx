ee7ba3408a480aad1de6a09b8add5498cddf9077
Eugene Chu <eugenchu@gmail.com>
2021-01-23 16:44:11 -0800

    fix up some more CSS to look like target's website

--- client/component/ColorOpt.jsx
+++ client/component/ColorOpt.jsx
@@ -37,25 +37,51 @@ const ColorBorders = style(ColorButton)`
     };
 `;
 const ColorSquare = style.div`
-height: 40px;
-width: 40px;
-background: ${props => props.color};
-padding: 2px;
-box-shadow: none;
-cursor: pointer;
+    height: 38px;
+    width: 38px;
+    background: ${props => props.color};
+    padding: 2px;
+    box-shadow: none;
+    cursor: pointer;
+    border-radius: 2px;
+    border: 1px solid gray;
+`;
+
+const Title = style.div`
+    font-size: 19px;
+    font-weight: bold;
+    text-align: left;
+`;
+
+const CurrColor = style.span`
+    font-weight: normal;
+`;
+
+const OutOfStock = style.div`
+    display: ${props => props.stock > 0 ? 'none' : 'block'};
+    width: 40px;
+    position: relative;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    background-color: rgba(255, 255, 255, 0.95);
+    z-index: 2;
+    font-size: 10px;
+    line-height: 1;
+    padding: 4px;
 `;
 
 var ColorOpt = (props) => {
     let currColor = props.currColor[0].toUpperCase()+props.currColor.slice(1);
     return (
         <Variations>
-        <div>Color: {currColor}</div>
+        <Title>color: <CurrColor>{currColor}</CurrColor></Title>
         <ColorVar>
 
             <ColorBorders selected={true}><ColorSquare color={props.currColor} key={props.currColor+1}/></ColorBorders>
             {
                 props.otherColors.map((colors, i) => {
-                    return <ColorBorders><ColorSquare color={colors.color} key={colors.color+i}/></ColorBorders>
+                    return <ColorBorders><ColorSquare color={colors.color} key={colors.color+i}><OutOfStock stock={colors.stock}>Out of Stock</OutOfStock></ColorSquare></ColorBorders>
                 })
             }
         </ColorVar>
