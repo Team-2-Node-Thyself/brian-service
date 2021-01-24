01ef2df5ce427128ea1245175e02fec6fd70261e
Eugene Chu <eugenchu@gmail.com>
2021-01-23 16:19:30 -0800

    add borders onto ColorOptions

--- client/component/ColorOpt.jsx
+++ client/component/ColorOpt.jsx
@@ -1,17 +1,48 @@
 import React from 'react';
-import style from 'styled-components';
+import style, {css} from 'styled-components';
 
 const Variations = style.div`
+    margin: 15px 0px;
 `;
 
 const ColorVar = style.div`
 display: flex;
+margin-top: 10px;
 justify-content: space-evenly;
 `;
-const ColorSquare = style.span`
+
+const ColorButton = style.button`
+    width: 52px;
+    height: 52px;
+    border-radius: 3px;
+    border-width: 2px;
+    overflow: visible;
+
+
+`;
+
+const ColorBorders = style(ColorButton)`
+    border-radius: 3px;
+    padding: 2px;
+    background: white;
+    border: ${props => props.selected ? '2px solid rgb(0, 131, 0)' :'2px solid transparent'};
+    &:hover{
+        border: 2px solid rgb(0, 131, 0);
+    };
+    &:focus{
+        color: rgb(51, 51, 51);
+        outline: rgb(136, 136, 136) dashed 1px;
+        outline-offset: 2px;
+        text-decoration: underline;
+    };
+`;
+const ColorSquare = style.div`
 height: 40px;
 width: 40px;
 background: ${props => props.color};
+padding: 2px;
+box-shadow: none;
+cursor: pointer;
 `;
 
 var ColorOpt = (props) => {
@@ -20,10 +51,11 @@ var ColorOpt = (props) => {
         <Variations>
         <div>Color: {currColor}</div>
         <ColorVar>
-            <ColorSquare color={props.currColor} key={props.currColor+1}/>
+
+            <ColorBorders selected={true}><ColorSquare color={props.currColor} key={props.currColor+1}/></ColorBorders>
             {
                 props.otherColors.map((colors, i) => {
-                    return <ColorSquare color={colors.color} key={colors.color+i}/>
+                    return <ColorBorders><ColorSquare color={colors.color} key={colors.color+i}/></ColorBorders>
                 })
             }
         </ColorVar>
