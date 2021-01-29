58ac72de743dae319e6420461e61c278563eee42
Eugene Chu <eugenchu@gmail.com>
2021-01-29 15:01:52 -0800

    Made initial get request easier to test without the proxy-server. And added compression to the server

--- server/index.js
+++ server/index.js
@@ -3,13 +3,15 @@ const parser = require('body-parser');
 const path = require('path');
 const db = require('./database');
 const cors = require('cors');
+const compression = require('compression');
 
 const PORT= 8003;
 const app = express();
 
 app.use(cors({
     origin: 'http://localhost:8000'
-}))
+}));
+app.use(compression());
 app.use(parser.json());
 app.use('/', express.static(path.join(__dirname, '..', 'public')));
 app.use('/bundle', express.static('public/bundle.js'))
