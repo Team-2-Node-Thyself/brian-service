aeea047a524d7b2be5b3d515eaafe81419de0983
Brian <bschaaf1017@gmail.com>
2021-02-05 16:54:44 -0500

    delete unused files

--- pgDatabase/index.js
+++
@@ -1,6 +0,0 @@
-const Sequelize = require('sequelize');
-
-const sequalize = new Sequelize("general_product_info_prod", "postgres", "Dhhsotnicam1", {host: "localhost", dialect: "postgres"});
-
-
-module.exports = sequalize;
