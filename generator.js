9e347626787b262f1875b83089c551f5e431b55f
Brian <bschaaf1017@gmail.com>
2021-02-05 16:50:42 -0500

    seperated generation and seeding from creation of mnodels

--- generator.js
+++ generator.js
@@ -1,12 +1,16 @@
-const fs = require('fs');
+const sequelizeStream = require('sequelize-stream')
 const faker = require('faker');
-const { Product, Colors } = require('./pgDatabase/model');
+const { Product, Colors, connection } = require('./pgDatabase/model');
 
-// const products = [];
+//open a stream to db
+sequelizeStream(connection);
+// stream.on('data', ({instance, event}) => console.log(event, instance.toJSON()));
+
+//generate product data and insert it to the products table
 const products = async () => {
-  for (let i = 0; i < 100; i++) {
+  for (let i = 0; i < 1000; i++) {
     let prod = [];
-    for (let j = 0; j < 100; j++) {
+    for (let j = 0; j < 1000; j++) {
       let product = {
         prodName: faker.commerce.productName(),
         price: faker.finance.amount(10, 300, 2),
@@ -15,27 +19,26 @@ const products = async () => {
       prod.push(product);
     }
     await Product.bulkCreate(prod, {returning: false})
+      .catch(err => console.log(err));
   }
-}
-products()
+};
+products();
 
-// fs.writeFileSync('products.json', JSON.stringify(products));
-
-// const colors = [];
+//generate color data and insert it into the Colors table
 const colors = async () => {
-  for (let i = 0; i < 100; i++) {
+  for (let i = 0; i < 1000; i++) {
     let prodColors = [];
-    for (let j = 0; j < 100; j++) {
+    for (let j = 0; j < 1000; j++) {
       let color1 = {
         color: faker.commerce.color(),
         qty: Math.floor(Math.random() * 30),
-        prod_Id: 10 * i + j + 1
+        prodnum: 10 * i + j + 1
       };
       prodColors.push(color1);
       let color2 = {
         color: faker.commerce.color(),
         qty: Math.floor(Math.random() * 30),
-        prodId: 10 * i + j + 1
+        prodnum: 10 * i + j + 1
       };
       prodColors.push(color2);
     }
@@ -43,12 +46,5 @@ const colors = async () => {
   }
 }
 colors()
-
-// fs.writeFileSync('colors.json', JSON.stringify(colors));
-
-// for (let i = 0; i < 4000; i++) {
-//   const items = Array.from({ length: 1000 }).map((_) => ({ tenant_id: faker.random.uuid() }));
-//   await Item.bulkCreate(items, { benchmark: true, returning: false });
-// }
 
 
