717253be30452baac4fbd76644c2fa4d2e04ab3d
Brian <bschaaf1017@gmail.com>
2021-02-09 18:47:45 -0500

    db

---
+++ dbConfig.js
@@ -0,0 +1,7 @@
+const DB_USER_NAME = 'postgres';
+const DB_PW = 'dhhsotnicam1';
+const DB_HOST = '3.141.86.21';
+const DB_PORT = 5432;
+const DB_DB = 'general_product_info_prod';
+
+module.exports = {DB_USER_NAME, DB_PW, DB_HOST, DB_PORT, DB_DB};
